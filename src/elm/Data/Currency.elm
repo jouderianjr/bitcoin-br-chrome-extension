8a9e205ace34266250d1691125c8d95cef33a867
Jouderian Nobre <jouderianjr@gmail.com>
2017-10-10 16:04:02 -0300

    Format code using elm-format :heart:

--- src/elm/Data/Currency.elm
+++ src/elm/Data/Currency.elm
@@ -1,64 +1,101 @@
 module Data.Currency exposing (..)
-import Json.Decode exposing (
-  Decoder,
-  string,
-  succeed,
-  field,
-  at,
-  list,
-  keyValuePairs,
-  map, float)
-import Json.Decode.Pipeline exposing (decode, required, hardcoded)
+
+import Json.Decode
+    exposing
+        ( Decoder
+        , at
+        , field
+        , float
+        , keyValuePairs
+        , list
+        , map
+        , string
+        , succeed
+        )
+import Json.Decode.Pipeline exposing (decode, hardcoded, required)
 import List
 
+
 type alias Currency =
-  { high: Float
-  , low: Float
-  , vol: Float
-  , last: Float
-  , exchange: String
-  , link: String
-  }
+    { high : Float
+    , low : Float
+    , vol : Float
+    , last : Float
+    , exchange : String
+    , link : String
+    }
+
 
 currenciesDecoder : Decoder (List Currency)
 currenciesDecoder =
-      at [ "ticker_12h", "exchanges" ]
-        ( itemDecoder
-           |> keyValuePairs
-           |> Json.Decode.map (\a -> List.map transformToCurrencyList  a ) )
+    at [ "ticker_12h", "exchanges" ]
+        (itemDecoder
+            |> keyValuePairs
+            |> Json.Decode.map (\a -> List.map transformToCurrencyList a)
+        )
 
-transformToCurrencyList : (String, Currency) -> Currency
-transformToCurrencyList (key, currency) =
-  {currency | exchange = getFullExchangeName key, link = getExchangeLink key}
+
+transformToCurrencyList : ( String, Currency ) -> Currency
+transformToCurrencyList ( key, currency ) =
+    { currency | exchange = getFullExchangeName key, link = getExchangeLink key }
+
 
 getFullExchangeName : String -> String
 getFullExchangeName abbr =
-  case abbr of
-    "NEG" -> "NegocieCoins"
-    "MBT" -> "MercadoBitcoin"
-    "FOX" -> "FoxBit"
-    "B2U" -> "BitcoinToYou"
-    "ARN" -> "Arena Bitcoin"
-    "LOC" -> "LocalBitcoins"
-    _ -> abbr
+    case abbr of
+        "NEG" ->
+            "NegocieCoins"
+
+        "MBT" ->
+            "MercadoBitcoin"
+
+        "FOX" ->
+            "FoxBit"
+
+        "B2U" ->
+            "BitcoinToYou"
+
+        "ARN" ->
+            "Arena Bitcoin"
+
+        "LOC" ->
+            "LocalBitcoins"
+
+        _ ->
+            abbr
+
 
 getExchangeLink : String -> String
 getExchangeLink abbr =
-  case abbr of
-    "NEG" -> "https://www.negociecoins.com.br"
-    "MBT" -> "https://www.mercadobitcoin.com.br"
-    "FOX" -> "https://foxbit.exchange"
-    "B2U" -> "https://www.bitcointoyou.com"
-    "ARN" -> "https://www.arenabitcoin.com.br"
-    "LOC" -> "https://localbitcoins.com/"
-    _ -> "#"
+    case abbr of
+        "NEG" ->
+            "https://www.negociecoins.com.br"
+
+        "MBT" ->
+            "https://www.mercadobitcoin.com.br"
+
+        "FOX" ->
+            "https://foxbit.exchange"
+
+        "B2U" ->
+            "https://www.bitcointoyou.com"
+
+        "ARN" ->
+            "https://www.arenabitcoin.com.br"
+
+        "LOC" ->
+            "https://localbitcoins.com/"
+
+        _ ->
+            "#"
+
 
 itemDecoder : Decoder Currency
 itemDecoder =
-  decode Currency
-    |> required "high" float
-    |> required "low" float
-    |> required "vol" float
-    |> required "last" float
-    |> hardcoded ""
-    |> hardcoded ""
+    decode Currency
+        |> required "high" float
+        |> required "low" float
+        |> required "vol" float
+        |> required "last" float
+        |> hardcoded ""
+        |> hardcoded ""
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,111 +1,148 @@
 module Main exposing (..)
 
-import Html exposing (
-  Html,
-  program,
-  div,
-  text,
-  button,
-  table,
-  thead,
-  tbody,
-  tr,
-  th,
-  td,
-  h3,
-  a)
-
+import Data.Currency exposing (Currency, currenciesDecoder)
+import Html
+    exposing
+        ( Html
+        , a
+        , button
+        , div
+        , h3
+        , program
+        , table
+        , tbody
+        , td
+        , text
+        , th
+        , thead
+        , tr
+        )
+import Html.Attributes exposing (class, href, target)
+import Http
 import List exposing (map)
 import String
 
-import Html.Attributes exposing (class, target, href)
-import Http
-
-import Data.Currency exposing (currenciesDecoder, Currency)
 
 -- APP
+
+
 main : Program Never Model Msg
-main = program
-  { init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
+
+
 type alias Model =
-  { currencies: List Currency
-  , err: String
-  }
+    { currencies : List Currency
+    , err : String
+    }
+
 
 model : Model
-model = {currencies = [], err = ""}
+model =
+    { currencies = [], err = "" }
+
+
 
 -- UPDATE
-type Msg = NoOp
-  | OnBtnClicked
-  | GetCurrencies (Result Http.Error (List Currency))
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg {currencies, err} =
-  case msg of
-    NoOp -> (model, Cmd.none)
-    OnBtnClicked ->
-      ({model | currencies = []}, getCurrencies)
-    GetCurrencies (Ok currencies) ->
-      ({model | currencies = currencies } , Cmd.none)
-    GetCurrencies (Err err) ->
-      ({model | err = toString err}, Cmd.none)
+
+type Msg
+    = NoOp
+    | OnBtnClicked
+    | GetCurrencies (Result Http.Error (List Currency))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg { currencies, err } =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        OnBtnClicked ->
+            ( { model | currencies = [] }, getCurrencies )
+
+        GetCurrencies (Ok currencies) ->
+            ( { model | currencies = currencies }, Cmd.none )
+
+        GetCurrencies (Err err) ->
+            ( { model | err = toString err }, Cmd.none )
+
+
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
-view {currencies} =
-  div
-    [ class "container" ]
-    [ h3 [ class "text-center" ] [ text "BR Bitcoin Currency" ]
-    , table [ class "table table-inverse" ]
-      [ thead []
-        [ tr []
-          [ th [] [ text "Exchange" ]
-          , th [] [ text "High" ]
-          , th [] [ text "Last" ]
-          , th [] [ text "Low" ]
-          , th [] [ text "Vol" ]
-          ]
+view { currencies } =
+    div
+        [ class "container" ]
+        [ h3 [ class "text-center" ] [ text "BR Bitcoin Currency" ]
+        , table [ class "table table-inverse" ]
+            [ thead []
+                [ tr []
+                    [ th [] [ text "Exchange" ]
+                    , th [] [ text "High" ]
+                    , th [] [ text "Last" ]
+                    , th [] [ text "Low" ]
+                    , th [] [ text "Vol" ]
+                    ]
+                ]
+            , tbody [] (map renderCurrencyRow currencies)
+            ]
         ]
-      , tbody [] (map renderCurrencyRow currencies)
-      ]
-    ]
+
 
 renderCurrencyRow : Currency -> Html Msg
 renderCurrencyRow currency =
-  tr []
-    [ th [] [ a [ target "_blank", href currency.link] [ text currency.exchange ] ]
-    , th [] [ text (formattedCurrency currency.high) ]
-    , th [] [ text (formattedCurrency currency.last) ]
-    , th [] [ text (formattedCurrency currency.low) ]
-    , th [] [ currency.vol |> toString |> text ]
-    ]
+    tr []
+        [ th [] [ a [ target "_blank", href currency.link ] [ text currency.exchange ] ]
+        , th [] [ text (formattedCurrency currency.high) ]
+        , th [] [ text (formattedCurrency currency.last) ]
+        , th [] [ text (formattedCurrency currency.low) ]
+        , th [] [ currency.vol |> toString |> text ]
+        ]
+
 
 formattedCurrency : Float -> String
 formattedCurrency number =
-  number
-    |> toString
-    |> String.append "R$ "
+    number
+        |> toString
+        |> String.append "R$ "
+
+
 
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
+
+
 
 -- INIT
-init : (Model, Cmd Msg)
-init = ( model , getCurrencies )
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, getCurrencies )
+
 
 getCurrencies : Cmd Msg
 getCurrencies =
-  let
-    url = "https://api.bitvalor.com/v1/ticker.json"
-  in
+    let
+        url =
+            "https://api.bitvalor.com/v1/ticker.json"
+    in
     Http.send GetCurrencies (Http.get url currenciesDecoder)
